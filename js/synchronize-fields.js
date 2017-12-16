b8b08e6ec8462013141b8cd9bdff93934508429c
Ulia Dumler <uliadumler@gmail.com>
2017-12-17 01:00:21 +0400

    Travis

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,7 +2,7 @@
 
 (function () {
   window.synchronizeFields = function (firstElement, secondElement, valuesOfFirstElement, valuesOfSecondElement, action) {
-    firstElement.addEventListener('change', function() {
+    firstElement.addEventListener('change', function () {
       for (var i = 0; i < valuesOfFirstElement.length; i++) {
         if (valuesOfFirstElement[i] === firstElement.value) {
           action(secondElement, valuesOfSecondElement[i]);
